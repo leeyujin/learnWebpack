247e274bfa4424ab9c5fa28f9839b90e27dd938b
yujin <now8567@hanamail.net>
2021-08-09 16:38:17 +0900

    Initial commit

---
+++ npm/getting-started/webpack.config.js
@@ -0,0 +1,11 @@
+// node path library load
+var path = require('path');
+
+module.exports = {
+    mode: 'none',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
